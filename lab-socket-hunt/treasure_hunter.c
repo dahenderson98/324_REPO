af07b031d77bb5485ec049a472b38a13fac2a473
Dallin Alexander Henderson <henderda@glacier.cs.byu.edu>
2023-03-06 22:10:33 -0700

    passing level 1

--- lab-socket-hunt/treasure_hunter.c
+++ lab-socket-hunt/treasure_hunter.c
@@ -46,12 +46,12 @@ int main(int argc, char *argv[]) {
 
 	struct sockaddr_in remote_addr_in;
 	struct sockaddr_in6 remote_addr_in6;
+	struct sockaddr *remote_addr;
 	
 	struct sockaddr_in local_addr_in;
 	struct sockaddr_in6 local_addr_in6;
 	struct addrinfo hints;
 	struct addrinfo *result;
-	struct sockaddr *remote_addr;
 	struct sockaddr *local_addr;
 
 	unsigned short remote_port;
@@ -77,36 +77,6 @@ int main(int argc, char *argv[]) {
 
 	sfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
 
-	// if (sfd == -1)
-		// continue;
-
-	/* Retrieve the values for the following, from the current
-		* result (rp) of getaddrinfo():
-		*
-		* addr_fam: the address family in use (applies to both local
-		*         and remote address).  This is either AF_INET (IPv4)
-		*         or AF_INET6 (IPv6).
-		* addr_len: length of the structure used to hold the address
-		*         (different for IPv4 and IPv6)
-		* remote_addr_in or remote_addr_in6: remote address and remote
-		*         port for IPv4 (addr_fam == AF_INET) or
-		*         IPv6 (addr_fam == AF_INET6).  The address is stored
-		*         in sin_addr (IPv4) or sin6_addr (IPv6), and the
-		*         port is stored in sin_port (IPv4) or sin6_port
-		*         (IPv6).
-		* remote_addr_str: string representation of remote IP address
-		* remote_addr: a struct sockaddr * that points to
-		*         remote_addr_in (where addr_fam == AF_INET) or
-		*         remote_addr_in6 (where addr_fam == AF_INET6)
-		* local_addr: a struct sockaddr * that points to
-		*         local_addr_in (where addr_fam == AF_INET) or
-		*         local_addr_in6 (where addr_fam == AF_INET6)
-		*         (Note that the *value* of local_addr_in or
-		*         local_addr_in6 will be populated later; for now
-		*         local_addr is simply assigned to point to one of
-		*         them.)
-		* remote_port: the remote port
-		* */
 	addr_fam = result->ai_family;
 	addr_len = result->ai_addrlen;
 	if (addr_fam == AF_INET) {
@@ -123,16 +93,7 @@ int main(int argc, char *argv[]) {
 		remote_addr = (struct sockaddr *)&remote_addr_in6;
 		local_addr = (struct sockaddr *)&local_addr_in6;
 	}
-	// fprintf(stderr, "Connecting to %s:%d (family: %d, len: %d)\n", remote_addr_str, remote_port, addr_fam, addr_len);
-
-	/* if connect is successful, then break out of the loop; we
-		* will use the current address */
-	// if (connect(sfd, remote_addr, addr_len) != -1)
-	// 	break;  /* Success */
-
-	// close(sfd);
 	
-
 	if (result == NULL) {   /* No address succeeded */
 		fprintf(stderr, "Could not connect\n");
 		exit(EXIT_FAILURE);
@@ -140,19 +101,6 @@ int main(int argc, char *argv[]) {
 
 	freeaddrinfo(result);   /* No longer needed */
 
-	/* Retrieve the following values using getsockname()
-	 *
-	 * local_addr_in or local_addr_in6: local address and local
-	 *         port for IPv4 (addr_fam == AF_INET) or
-	 *         IPv6 (addr_fam == AF_INET6).  The address is stored
-	 *         in sin_addr (IPv4) or sin6_addr (IPv6), and the
-	 *         port is stored in sin_port (IPv4) or sin6_port
-	 *         (IPv6).  Note that these are populated with the call
-	 *         to getsockname() because local_addr points to
-	 *         struct corresponding to the address family.
-	 * local_addr_str: string representation of local IP address
-	 * local_port: the local port
-	 * */
 	s = getsockname(sfd, local_addr, &addr_len);
 	if (addr_fam == AF_INET) {
 		inet_ntop(addr_fam, &local_addr_in.sin_addr,
@@ -163,8 +111,6 @@ int main(int argc, char *argv[]) {
 				local_addr_str, addr_len);
 		local_port = ntohs(local_addr_in6.sin6_port);
 	}
-	// fprintf(stderr, "Local socket info: %s:%d (family: %d, len: %d)\n", local_addr_str, local_port, addr_fam, addr_len);
-
 
 	/**** Level 0 *****/
 
@@ -203,12 +149,18 @@ int main(int argc, char *argv[]) {
 	t_ptr += chunk_l_0;
 	// printf("%s\n", treasure);
 
-	// int op_code_0 = response_0[chunk_l_0 + 1];
+	int op_code_0 = response_0[chunk_l_0 + 1];
 	// printf("%d\n", op_code_0);
 
 	unsigned short op_param_0;
 	memcpy(&op_param_0, &response_0[chunk_l_0 + 2], 2);
+	unsigned short op_param_rev_0 = ntohs(op_param_0);
 	// printf("%x\n", op_param_0);
+
+	// Op-Code 1 -> update remote port
+	if (op_code_0 == 1) {
+		remote_addr_in.sin_port = htons(op_param_rev_0);
+	}
 
 	unsigned int nonce_0;
 	memcpy(&nonce_0, &response_0[chunk_l_0 + 4], 4);
@@ -260,6 +212,14 @@ int main(int argc, char *argv[]) {
 		unsigned short op_param;
 		memcpy(&op_param, &response[chunk_l + 2], 2);
 		// printf("%x\n", op_param_0);
+
+		unsigned short op_param_rev = ntohs(op_param);
+		// printf("%x\n", op_param);
+
+		// Op-Code 1 -> update remote port
+		if (op_code == 1) {
+			remote_addr_in.sin_port = htons(op_param_rev);
+		}
 
 		unsigned int nonce;
 		memcpy(&nonce, &response[chunk_l + 4], 4);
