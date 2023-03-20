71eb21c83be68fabfac9a6ec3bd8fc3b7559fee5
Dallin Alexander Henderson <henderda@loki.cs.byu.edu>
2023-03-20 13:19:27 -0600

    Parser works

--- lab-proxy-threadpool/proxy.c
+++ lab-proxy-threadpool/proxy.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
+#define MAX_LINE 256
 
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0";
 
@@ -14,18 +16,65 @@ void print_bytes(unsigned char *, int);
 
 int main()
 {
-	test_parser();
+	// test_parser();
 	printf("%s\n", user_agent_hdr);
 	return 0;
 }
 
 int all_headers_received(char *request) {
-	return 0;
+	return !strstr(request, "\r\n\r\n") ? 0 : 1;
 }
 
 int parse_request(char *request, char *method,
 		char *hostname, char *port, char *path, char *headers) {
-	return 0;
+
+	// Ensure all buffers are empty
+	int m_l = sizeof(method);
+	memset(&method[0], 0, m_l);
+	int ho_l = sizeof(hostname);
+	memset(&hostname[0], 0, ho_l);
+	int po_l = sizeof(port);
+	memset(&port[0], 0, po_l);
+	int pa_l = sizeof(path);
+	memset(&path[0], 0, pa_l);
+	int he_l = sizeof(headers);
+	memset(&headers[0], 0, he_l);
+
+	// Get request method
+	char *first_sp = strchr(request, ' ');
+	strncpy(method, request, first_sp - request);
+
+	// Get request URL
+	char url[MAX_LINE];
+	char *sec_sp = strchr(first_sp + 1, ' ');
+	strncpy(url, first_sp + 1, (sec_sp - 1) - first_sp);
+	url[(sec_sp - 1) - first_sp] = 00;
+
+	// From URL, get hostname, port, and path
+	char *slashes = strstr(url, "//");
+	char *sec_colon = strchr(slashes + 2, ':');
+	char *third_fs = strchr(slashes + 2, '/');
+	// If no port specified, port is set to 80 by default
+	if (!sec_colon) {
+		strncpy(hostname, slashes + 2, (third_fs - 1) - (slashes + 1));
+		strcpy(port, "80");
+	}
+	// Else, parse port after hostname
+	else {
+		strncpy(hostname, slashes + 2, (sec_colon - 1) - (slashes + 1));
+		hostname[(sec_colon - 1) - (slashes + 1)] = 00;
+		strncpy(port, sec_colon + 1, third_fs - (sec_colon + 1));
+		port[third_fs - (sec_colon + 1)] = 00;
+
+	}
+
+	// Get path from end of URL
+	strcpy(path, third_fs);
+
+	char *end_f_line = strstr(request, "\r\n");
+	strcpy(headers, end_f_line + 2);
+	
+	return 1;
 }
 
 void test_parser() {
